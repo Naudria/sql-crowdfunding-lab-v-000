ba0b535e1496e5ebabeb9f45a3a06a69edf7cd83
Rima Jean <rima.jean@gmail.com>
2018-11-05 17:32:17 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ ON users.id = pledges.user_id GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) * -1 AS over_goal FROM projects
+  "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount))
     INNER JOIN pledges
       ON projects.id = pledges.project_id
     GROUP BY projects.title
